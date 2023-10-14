66cf6ace6135a8e389c1078d499fa0c70ad50787
junha <goodjunha@gmail.com>
2023-10-14 15:16:45 +0900

    🖤:: tuist 세팅

---
+++ Targets/BlendingIOSKit/Tests/BlendingIOSKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class BlendingIOSKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("BlendingIOSKit", "BlendingIOSKit")
+    }
+}
---
+++ Targets/BlendingIOSUI/Tests/BlendingIOSUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class BlendingIOSUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("BlendingIOSUI", "BlendingIOSUI")
+    }
+}
