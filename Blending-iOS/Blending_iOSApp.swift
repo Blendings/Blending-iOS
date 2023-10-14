47473cab8072ab1b4fe43e19940581e62c827789
junha <goodjunha@gmail.com>
2023-10-14 17:04:54 +0900

    🖤:: swiftUI 파일 추가

---
+++ Blending-iOS/Blending_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blending_iOSApp.swift
+//  Blending-iOS
+//
+//  Created by 박준하 on 10/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Blending_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainTapView()
+        }
+    }
+}
---
+++ Blending-iOS/MainTapView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct MainTapView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MainTapView()
+}
