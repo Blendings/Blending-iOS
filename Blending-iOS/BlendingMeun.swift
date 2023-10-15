6094e234b13068c5b9a06d4f49525780c82fb779
junha <goodjunha@gmail.com>
2023-10-15 12:41:48 +0900

    🧡:: 이름 리펙토링

--- Blending-iOS/BlendingMeun.swift
+++ Blending-iOS/BlendingMeun.swift
@@ -14,11 +14,11 @@ public struct BlendingMeun: Identifiable {
     public let id = UUID()
     
     static let sample: [BlendingMeun] = [
-        BlendingMeun(image: Image("sampleImage1"), name: "샘플 이미지"),
-        BlendingMeun(image: Image("sampleImage1"), name: "샘플 이미지"),
-        BlendingMeun(image: Image("sampleImage1"), name: "샘플 이미지"),
-        BlendingMeun(image: Image("sampleImage1"), name: "샘플 이미지"),
-        BlendingMeun(image: Image("sampleImage1"), name: "샘플 이미지"),
-        BlendingMeun(image: Image("sampleImage1"), name: "샘플 이미지")
+        BlendingMeun(image: Image("sampleImage1"), name: "샘플 이미지1"),
+        BlendingMeun(image: Image("sampleImage1"), name: "샘플 이미지2"),
+        BlendingMeun(image: Image("sampleImage1"), name: "샘플 이미지3"),
+        BlendingMeun(image: Image("sampleImage1"), name: "샘플 이미지4"),
+        BlendingMeun(image: Image("sampleImage1"), name: "샘플 이미지5"),
+        BlendingMeun(image: Image("sampleImage1"), name: "샘플 이미지6")
     ]
 }
