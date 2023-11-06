1aad641b28fd197bef6e57e10c5190f11b1991aa
junha <goodjunha@gmail.com>
2023-11-06 19:39:08 +0900

    🖤:: extension 추가

--- Blending-iOS/Util/DateFormatter+Extension.swift
+++ Blending-iOS/Util/DateFormatter+Extension.swift
@@ -5,4 +5,19 @@
 //  Created by 박준하 on 10/30/23.
 //
 
+
 import Foundation
+
+extension DateFormatter {
+    func toYearMonthDay(date: Date) -> String {
+        self.dateFormat = "yyyy.MM.dd"
+        self.locale = Locale(identifier: "ko_KR")
+        return self.string(from: date)
+    }
+    
+    func toYearMonthDayWeek(date: Date) -> String {
+        self.dateFormat = "yyyy.MM.dd(EEE)"
+        self.locale = Locale(identifier: "ko_kr")
+        return self.string(from: date)
+    }
+}
