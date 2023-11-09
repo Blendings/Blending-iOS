e747124fe095fd70aa2e809236863565b91a82fb
junha <goodjunha@gmail.com>
2023-11-09 19:02:04 +0900

    🖤:: 로그인 뷰 추가

---
+++ Blending-iOS/Scense/Start/LoginView.swift
@@ -0,0 +1,39 @@
+//
+//  LoginView.swift
+//  Blending-iOS
+//
+//  Created by 박준하 on 11/9/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        ScrollView(.vertical) {
+            VStack(alignment: .leading, spacing: 20) {
+                CustomAuthTextField(placeholder: "이메일", text: $email)
+                    .keyboardType(.emailAddress)
+                CustomAuthTextField(placeholder: "패스워드", text: $password)
+                
+                HStack {
+                    Spacer()
+                    Button(action: {
+                        print("email: \(email)\npassword: \(password)")
+                    }) {
+                        Text("로그인")
+                            .font(.title)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
+                    Spacer()
+                }
+            }
+            .padding()
+        }
+    }
+}
