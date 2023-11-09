c8d998081cee8878ddc78e64698b6b6599158c5c
junha <goodjunha@gmail.com>
2023-11-09 19:47:29 +0900

    🧡:: imagePickerView 추가

---
+++ Blending-iOS/DesignSystem/ImagePicker.swift
@@ -0,0 +1,46 @@
+import SwiftUI
+import UIKit
+
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var selectedImage: Image?
+    @Environment(\.presentationMode) private var presentationMode
+    private var sourceType: UIImagePickerController.SourceType
+    
+    init(selectedImage: Binding<Image?>, sourceType: UIImagePickerController.SourceType = .photoLibrary) {
+        _selectedImage = selectedImage
+        self.sourceType = sourceType
+    }
+    
+    class Coordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
+            if let uiImage = info[.originalImage] as? UIImage {
+                parent.selectedImage = Image(uiImage: uiImage)
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        picker.sourceType = sourceType
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+    }
+}
